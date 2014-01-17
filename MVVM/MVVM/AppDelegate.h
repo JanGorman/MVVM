629d771b33e90b699c0de79c0ec562f46e366b4a
Jan Gorman <gorman.jan@gmail.com>
2014-01-17 09:31:18 +0100

    Initial project setup

---
+++ MVVM/MVVM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by Sir Jan Gorman on 17.01.14.
+//  Copyright (c) 2014 Jan Gorman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MVVM/MVVM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MVVM
+//
+//  Created by Sir Jan Gorman on 17.01.14.
+//  Copyright (c) 2014 Jan Gorman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
