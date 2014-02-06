0a82a02af1bb0a32e3ec2dab9db5a91ddf86e9a5
Jan Gorman <gorman.jan@gmail.com>
2014-02-06 09:35:50 +0100

    Add infinite scrolling to the collection view example

--- MVVM/MVVM/Controller/Collection/JGOCollectionViewController.m
+++ MVVM/MVVM/Controller/Collection/JGOCollectionViewController.m
@@ -12,10 +12,16 @@
 #import "JGOCollectionViewModel.h"
 #import "RACSignal+Operations.h"
 #import "UIImageView+AFNetworking.h"
+#import "UIScrollView+SVInfiniteScrolling.h"
+
+static const int kTriggerOffset = 10;
 
 @interface JGOCollectionViewController ()
 
 @property(strong, nonatomic) JGOCollectionViewModel *viewModel;
+
+@property(assign, nonatomic) NSUInteger currentPage;
+@property(strong, nonatomic) NSMutableSet *loadPoints;
 
 @end
 
@@ -27,6 +33,7 @@
     self = [super initWithCoder:coder];
     if (self) {
         _viewModel = [[JGOCollectionViewModel alloc] init];
+        _loadPoints = [NSMutableSet set];
 
         [[RACSignal merge:@[[_viewModel fetchCollection]]] subscribeCompleted:^{
             [self.collectionView reloadData];
@@ -39,13 +46,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self addInfiniteScrolling];
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return [self.viewModel numberOfItems];
 }
 
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    [self triggerInfiniteScrolling:indexPath];
+
     JGOCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([JGOCollectionViewCell class])
                                                                             forIndexPath:indexPath];
     cell.imageView.imageWithURL = [self.viewModel imageURLAtIndexPath:indexPath];
@@ -54,5 +66,24 @@
     return cell;
 }
 
+#pragma mark - Private
+
+- (void)addInfiniteScrolling {
+    __weak JGOCollectionViewController *weakSelf = self;
+    [self.collectionView addInfiniteScrollingWithActionHandler:^{
+        weakSelf.currentPage += 1;
+
+        [[RACSignal merge:@[[weakSelf.viewModel fetchCollection]]] subscribeCompleted:^{
+            [weakSelf.collectionView reloadData];
+        }];
+    }];
+}
+
+- (void)triggerInfiniteScrolling:(NSIndexPath *)indexPath {
+    if (indexPath.row % kTriggerOffset == 0 && ![self.loadPoints containsObject:@(indexPath.row)]) {
+        [self.loadPoints addObject:@(indexPath.row)];
+        [self.collectionView triggerInfiniteScrolling];
+    }
+}
 
 @end
